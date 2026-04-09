1b9be9d23018aff77ae8d4ca9508c8acf4692bdc
OnggiJonggi <maru403@naver.com>
2026-04-09 13:22:21 +0900

    초기 설정

---
+++ src/test/java/website/treelink/TreelinkApplicationTests.java
@@ -0,0 +1,13 @@
+package website.treelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreelinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
